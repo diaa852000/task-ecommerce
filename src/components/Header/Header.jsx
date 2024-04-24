0e51263a42f52947aef490c6a23ce5be75b15609
diaa <diaa2442000@gmail.com>
2024-04-24 22:51:27 +0200

    handle ui for cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,54 +3,71 @@ import navlinks from '../../fixtures/navlinks'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/a-logo.png'
 import cartIcon from '../../assets/cart.svg'
+import Cart from '../Cart/Cart'
+
 export default class Header extends Component {
     constructor() {
         super();
 
         this.state = {
             hoveredLinkId: null,
+            isOpenCart: false,
         };
 
         this.hoverNavLink = this.hoverNavLink.bind(this);
+        this.toggleCart = this.toggleCart.bind(this);
     };
 
-    hoverNavLink (linkId) {
-        this.setState({hoveredLinkId: linkId})
+    hoverNavLink(linkId) {
+        this.setState({ hoveredLinkId: linkId })
     };
 
+    toggleCart () {
+        this.setState(prevState => {
+            return {
+                isOpenCart: !prevState.isOpenCart
+            }
+        })
+    };
 
     render() {
         return (
-            <nav className='main-container grid grid-cols-7 h-[80px] text-base font-medium'>
-                <ul className='flex flex-row items-center gap-4 col-span-3'>
-                    {navlinks.map(navlink => (
-                        <li
-                            key={navlink.id}
-                            className={`uppercase ${ this.state.hoveredLinkId === navlink.id && 'active-link'}`}
-                            onMouseEnter={() => this.hoverNavLink(navlink.id)}
-                            onMouseLeave={() => this.hoverNavLink(null)}
-                            onClick={()=> this.hoverNavLink(navlink.id)}
-                        >
-                            <Link to={navlink.route}>{navlink.label}</Link>
-                        </li>
-                    ))}
+            <>
+                <nav className='main-container grid grid-cols-7 h-[80px] text-base font-medium z-30 bg-white'>
+                    <ul className='flex flex-row items-center gap-4 col-span-3'>
+                        {navlinks.map(navlink => (
+                            <li
+                                key={navlink.id}
+                                className={`uppercase ${this.state.hoveredLinkId === navlink.id && 'active-link'}`}
+                                onMouseEnter={() => this.hoverNavLink(navlink.id)}
+                                onMouseLeave={() => this.hoverNavLink(null)}
+                                onClick={() => this.hoverNavLink(navlink.id)}
+                            >
+                                <Link to={navlink.route}>{navlink.label}</Link>
+                            </li>
+                        ))}
+                    </ul>
 
-                </ul>
+                    <Link to={'/'} className='col-span-1 flex items-center justify-center'>
+                        <img
+                            src={logo}
+                            alt="logo"
+                        />
+                    </Link>
 
-                <Link to={'/'} className='col-span-1 flex items-center justify-center'>
-                    <img
-                        src={logo}
-                        alt="logo"
-                    />
-                </Link>
+                    <div className='col-span-3 flex items-center justify-self-end w-fit relative px-2'>
+                        <span className='absolute top-4 md:top-3.5 -right-0 bg-[#1d1f22] text-white text-[10px] size-4
+                        rounded-full flex justify-center items-center font-bold'>
+                            3
+                        </span>
+                        <button type='button' onClick={this.toggleCart}>
+                            <img src={cartIcon} alt="cartIcon" />
+                        </button>
+                    </div>
+                </nav>
 
-                <div className='col-span-3 flex items-center justify-self-end w-fit'>
-                    <button type='button'>
-                        <img src={cartIcon} alt="cartIcon" />
-                    </button>
-                </div>
-
-            </nav>
+                {this.state.isOpenCart && <Cart/>}
+            </>
         )
     }
 };
